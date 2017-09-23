6960e88333624c4437cac18214b6d7b31c65033c
DESKTOP-PV4VR8Q\40737 <shuo.niu@mail.utoronto.ca>
2017-09-23 11:18:05 -0400

    first commit

---
+++ cell.js
@@ -0,0 +1,91 @@
+﻿function Cell(i, j, w) {
+    this.i = i;
+    this.j = j;
+    this.x = i * w;
+    this.y = j * w;
+    this.w = w;
+    this.neighborCount = 0;
+
+    this.bee = false;
+    this.revealed = false;
+    this.marked = false;
+}
+
+Cell.prototype.show = function () {
+    stroke(0);  // no border
+    noFill();
+    rect(this.x, this.y, this.w, this.w);
+    if (this.revealed) {
+        if (this.bee && this.marked) {
+            fill(0,255,0);
+            ellipse(this.x + 0.5 * this.w, this.y + 0.5 * this.w, this.w * 0.5);
+        } else if (this.bee) {
+            fill(127);
+            ellipse(this.x + 0.5 * this.w, this.y + 0.5 * this.w, this.w * 0.5);
+        } else {
+            fill(200);
+            rect(this.x, this.y, this.w, this.w);
+            if (this.neighborCount > 0) {
+                textAlign(CENTER);
+                fill(0);
+                text(this.neighborCount, this.x + this.w * 0.5, this.y + this.w - 5);
+            }
+        }
+    } else if (this.marked) {
+        fill(255,0,0);
+        ellipse(this.x + 0.5 * this.w, this.y + 0.5 * this.w, this.w * 0.5);
+    }
+}
+
+Cell.prototype.countBees = function () {
+    if (this.bee) {
+        this.neighborCount = -1;
+        return;
+    }
+    var total = 0;
+    for (var xoff = -1; xoff < 2; xoff++) {
+        for (var yoff = -1; yoff < 2; yoff++) {
+            var i = this.i + xoff;
+            var j = this.j + yoff;
+            if (i > -1 && i < cols && j > -1 && j < rows) {
+                var neighbor = grid[i][j];
+                if (neighbor.bee) {
+                    total++;
+                }
+            }
+        }
+    }
+    this.neighborCount = total;
+}
+
+Cell.prototype.contains = function (x, y) {
+    return (x > this.x && x < this.x + this.w &&
+            y > this.y && y < this.y + this.w);
+}
+
+Cell.prototype.reveal = function () {
+    this.revealed = true;
+    if (this.neighborCount == 0) {
+        // flood fill
+        this.floodFill();
+    }
+}
+
+Cell.prototype.floodFill = function () {
+    for (var xoff = -1; xoff < 2; xoff++) {
+        for (var yoff = -1; yoff < 2; yoff++) {
+            var i = this.i + xoff;
+            var j = this.j + yoff;
+            if (i > -1 && i < cols && j > -1 && j < rows) {
+                var neighbor = grid[i][j];
+                if (!neighbor.bee && !neighbor.revealed) {
+                    neighbor.reveal();
+                }
+            }
+        }
+    }
+}
+
+Cell.prototype.mark = function () {
+    this.marked = !this.marked;
+}
---
+++ sketch.js
@@ -0,0 +1,86 @@
+﻿function make2DArray(cols, rows) {
+    var arr = new Array(cols);
+    for (var i = 0; i < arr.length; i++) {
+        arr[i] = new Array(rows);
+    }
+    return arr;
+}
+
+var grid;
+var cols;
+var rows;
+var w = 20;
+
+var totalBees = 20;
+
+function setup() {
+    createCanvas(201, 201);
+    cols = floor(width / w);
+    rows = floor(height / w);
+    grid = make2DArray(cols, rows);
+    for (var i = 0; i < cols; i++) {
+        for (var j = 0; j < rows; j++) {
+            grid[i][j] = new Cell(i, j, w);
+        }
+    }
+
+    // pick totalBees spots
+    for (var n = 0; n < totalBees; n++) {
+        var i = floor(random(cols));
+        var j = floor(random(rows));
+        if (!grid[i][j].bee) {
+            grid[i][j].bee = true;
+        } else {
+            n--;
+        }
+    }
+
+    for (var i = 0; i < cols; i++) {
+        for (var j = 0; j < rows; j++) {
+            grid[i][j].countBees();
+        }
+    }
+}
+
+function gameOver() {
+    for (var i = 0; i < cols; i++) {
+        for (var j = 0; j < rows; j++) {
+            grid[i][j].revealed = true;
+        }
+    }
+}
+
+function mousePressed() {
+    //background(0);
+    if (mouseButton == LEFT) {
+        for (var i = 0; i < cols; i++) {
+            for (var j = 0; j < rows; j++) {
+                if (grid[i][j].contains(mouseX, mouseY)) {
+                    grid[i][j].reveal();
+
+                    if (grid[i][j].bee) {
+                        gameOver();
+                    }
+                }
+            }
+        }
+    }
+    if (mouseButton == CENTER) {
+        for (var i = 0; i < cols; i++) {
+            for (var j = 0; j < rows; j++) {
+                if (grid[i][j].contains(mouseX, mouseY)) {
+                    grid[i][j].mark();
+                }
+            }
+        }
+    }
+}
+
+function draw() {
+    background(255);
+    for (var i = 0; i < cols; i++) {
+        for (var j = 0; j < rows; j++) {
+            grid[i][j].show();
+        }
+    }
+}
